f2e1b067948eb4d55c4863897d5d19872f4f5743
SangUk Park <parksang1993@gmail.com>
2023-11-13 15:57:31 +0900

    If-Else

---
+++ If-Else.playground/Contents.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let myName = "Steve"
+let myAge = 20
+let yourName = "Foo"
+let yourAge = 19
+
+if myName == "steve" {
+    "Your name is \(myName)"
+} else {
+    "Oops I guessed it wrong"
+}
