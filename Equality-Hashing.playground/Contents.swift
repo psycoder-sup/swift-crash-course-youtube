f682373a0adef22c51d297805b8e34acd94c9ef7
SangUk Park <parksang1993@gmail.com>
2023-11-16 15:42:06 +0900

    Equality-Hashing

---
+++ Equality-Hashing.playground/Contents.swift
@@ -0,0 +1,160 @@
+import Foundation
+
+
+struct Person: Equatable {
+    let id: String
+    let name: String
+}
+
+let foo1 = Person(
+    id: "1",
+    name: "Foo"
+)
+
+let foo2 = Person(
+    id: "1",
+    name: "Bar"
+)
+
+
+if foo1 == foo2 {
+    "They are equal"
+} else {
+    "they are not equal"
+}
+
+extension Person {
+    static func == (
+        lhs: Self,
+        rhs: Self
+    ) -> Bool {
+        lhs.id == rhs.id
+    }
+}
+
+enum AnimalType {
+    case dog(breed: String)
+    case cat(breed: String)
+}
+
+let dog1 = AnimalType.dog(breed: "Labradoodle")
+let dog2 = AnimalType.dog(breed: "Labradoodle")
+
+if dog1 == dog2 {
+    "They are same breed"
+} else {
+    "They are not the same breed"
+}
+
+extension AnimalType: Equatable {
+    static func == (
+        lhs: Self,
+        rhs: Self
+    ) -> Bool {
+        switch (lhs, rhs) {
+        case let (.dog(lhsBreed), .dog(rhsBreed)),
+            let (.cat(lhsBreed), .cat(rhsBreed)):
+            return lhsBreed == rhsBreed
+        default:
+            return false
+        }
+    }
+}
+
+struct Animal: Equatable {
+    let name: String
+    let type: AnimalType
+    
+    static func == (
+        lhs: Self,
+        rhs: Self
+    ) -> Bool {
+        lhs.type == rhs.type
+    }
+}
+
+let cat1 = Animal(name: "Whiskers", type: .cat(breed: "Street cat"))
+let cat2 = Animal(name: "Whoosh", type: .cat(breed: "Street cat"))
+
+if cat1 == cat2 {
+    "They are equal beause of their type"
+} else {
+    "They are not equal"
+}
+
+
+
+/// Hashable
+
+struct House: Hashable {
+    let number: Int
+    let numberOfBedrooms: Int
+}
+
+
+let house1 = House(number: 123, numberOfBedrooms: 2)
+house1.hashValue
+let house2 = House(number: 123, numberOfBedrooms: 3)
+house2.hashValue
+
+let houses = Set([house1, house2])
+houses.count
+
+
+struct NumberedHouse: Hashable {
+    let number: Int
+    let numberOfBedrooms: Int
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(number)
+    }
+    
+    static func == (
+        lhs: Self,
+        rhs: Self
+    ) -> Bool {
+        lhs.number == rhs.number
+    }
+}
+
+let house3 = NumberedHouse(number: 123, numberOfBedrooms: 2)
+let house4 = NumberedHouse(number: 123, numberOfBedrooms: 3)
+
+let houses3And4 = Set([house3, house4])
+houses3And4.count
+
+
+enum CarPart {
+    case roof
+    case tire
+    case trunk
+}
+
+let uniqueparts: Set<CarPart> = [
+    .roof,
+    .tire,
+    .trunk,
+    .roof
+]
+
+enum HouseType: Hashable {
+    case bigHouse(NumberedHouse)
+    case smallHouse(NumberedHouse)
+}
+
+let bigHouse1 = HouseType.bigHouse(NumberedHouse(number: 1, numberOfBedrooms: 1))
+let bigHouse2 = HouseType.bigHouse(NumberedHouse(number: 1, numberOfBedrooms: 1))
+let smallHouse = HouseType.smallHouse(NumberedHouse(number: 1, numberOfBedrooms: 1))
+
+let allHouses: Set<HouseType> = [
+    bigHouse1,
+    bigHouse2,
+    smallHouse
+]
+
+allHouses.count
+
+for value in allHouses {
+    print(value)
+}
+
